d90222dc66602bc3ddda3e15240b49cb1bd02871
Danny <dl667@cornell.edu>
2022-10-03 17:59:24 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// DEPENDENCIES
+var startButton = document.querySelector("#start-button");
+
+// DATA
+    // Questions and Answers
+    // Initially empty list of quiz takers and their scores
+
+// FUNCTIONS
+    // When "start" button is clicked, quiz and timer starts
+        // Timer needs separate function probably
+    // Determine whether answer selected is right or wrong
+        // If right, move on to next question
+        // If wrong, highlight red and deduct time from timer
+    // Display next question
+    // When user inputs initials, add to list of finished quizzes
+        // Order the list based on score/time
+
+// USER INTERACTIONS
+    // User clicks "start" to start the quiz and timer
+addEventListener("click", startGame());
+
+function startGame() {
+
+}
+
+    // User clicks button to choose their answer
+    // User clicks highscore link to display scoreboard
+    // User presses keys to input initials for score
+
+// INITIALIZATION
