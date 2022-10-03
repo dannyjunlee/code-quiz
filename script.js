a8d9da4b98f8ade896d2aee410aa210ea5bb1850
Danny <dl667@cornell.edu>
2022-10-03 18:07:18 -0400

    started writing out javascript for timer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 var startButton = document.querySelector("#start-button");
+var timerEl = document.getElementById("timer");
 
 // DATA
     // Questions and Answers
@@ -7,7 +8,25 @@ var startButton = document.querySelector("#start-button");
 
 // FUNCTIONS
     // When "start" button is clicked, quiz and timer starts
-        // Timer needs separate function probably
+        
+startButton.addEventListener("click", function() {
+
+});
+
+// Timer needs separate function probably
+function countdown() {
+    var timeLeft = 60;
+
+    var timeInteval = setInterval(function() {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+
+        // if (timeLeft === 0) {
+        //     clearInterval(timeInteval);
+        // }
+    }, 1000);
+}
+
     // Determine whether answer selected is right or wrong
         // If right, move on to next question
         // If wrong, highlight red and deduct time from timer
@@ -17,12 +36,6 @@ var startButton = document.querySelector("#start-button");
 
 // USER INTERACTIONS
     // User clicks "start" to start the quiz and timer
-addEventListener("click", startGame());
-
-function startGame() {
-
-}
-
     // User clicks button to choose their answer
     // User clicks highscore link to display scoreboard
     // User presses keys to input initials for score
